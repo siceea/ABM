6a3fc0f77b6ef5eccee78f3baa5144ae3e2cd99e
siceea <noceea@gmail.com>
2025-02-23 15:51:29 -0600

    Update delete.php
    Correccion de vulnerabilidad: TRUE palabra reserveda para mayor legibilidad

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ $id = $_GET['id'];
 
 $sql = "DELETE FROM usuarios WHERE id=$id";
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === true) {
     echo "Registro eliminado exitosamente";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
