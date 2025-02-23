fd33b703ab765cd524c3a7dfb20bc34307f79a0e
siceea <noceea@gmail.com>
2025-02-23 16:08:39 -0600

    Update delete.php
    Correccion de vulnerabilidad: XSS e Injection SQL

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'db_connect.php';
-
+/*
 $id = $_GET['id'];
 
 $sql = "DELETE FROM usuarios WHERE id=$id";
@@ -9,4 +9,23 @@ if ($conn->query($sql) === true) {
     echo "Registro eliminado exitosamente";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+}
+*/
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $stmt = $conn->prepare("DELETE FROM usuarios WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    
+    if ($stmt->execute()) {
+        echo "Registro eliminado exitosamente";
+    } else {
+        $mensaje= Error en la creacion desde la base de datos
+        echo htmlspecialchars($mensaje, ENT_QUOTES, 'UTF-8');
+        //echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+} else {
+    echo "ID no válido";
 }
